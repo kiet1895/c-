4d42b42012e246847775294a41f3a647913ab997
c++ <kiet1895@gmail.com>
2020-10-31 16:01:40 +0700

    add line 2

--- tinhtong.cpp
+++ tinhtong.cpp
@@ -5,7 +5,8 @@ int main()
     int a,b, tong=0;
     cout << "nhap a: "; cin >>a;
     cout << "nhap b: "; cin >>b;
-   tong= a + b;
-   cout << a << " + " << b << " = " << tong;
+    tong= a + b;
+    cout << a << " + " << b << " = " << tong;
+    cout <<"tong cua 2 so laf : " << tong;
    return 0;
 }
