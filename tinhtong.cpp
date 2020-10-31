2310b4856f2c119aefd2897bb2706fd3822e09eb
c++ <kiet1895@gmail.com>
2020-10-31 22:27:04 +0700

    comment

--- tinhtong.cpp
+++ tinhtong.cpp
@@ -7,6 +7,6 @@ int main()
     cout << "nhap b: "; cin >>b;
     tong= a + b;
     cout << a << " + " << b << " = " << tong;
-    cout <<"tong cua 2 so laf : " << tong;
+    // cout <<"tong cua 2 so laf : " << tong;
    return 0;
 }
