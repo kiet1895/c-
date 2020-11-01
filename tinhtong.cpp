2e864c4ca757ba4115b6c8d810d7ab341cd8a4c0
c++ <kiet1895@gmail.com>
2020-11-01 16:01:16 +0700

    so chẵn lẻ

--- tinhtong.cpp
+++ tinhtong.cpp
@@ -1,12 +1,66 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
+
+int sochan(){
+        int so=1;
+    while (so <= 100)
+    {
+        if(so % 2 == 1)
+        {
+            cout<< so << "\t";
+        }
+        if (so % 10 == 0)
+        {
+            cout<< "\n";
+        }
+        
+        so = so + 1;
+    }
+    return 0;
+}
+int sole(){
+        int so=1;
+    while (so <= 100)
+    {
+        if(so % 2 == 1)
+        {
+            cout<< so << "\t";
+        }
+        if (so % 10 == 0)
+        {
+            cout<< "\n";
+        }
+        
+        so = so + 1;
+    }
+    return 0;
+}
 int main()
 {
-    int a,b, tong=0;
-    cout << "nhap a: "; cin >>a;
-    cout << "nhap b: "; cin >>b;
-    tong= a + b;
-    cout << a << " + " << b << " = " << tong;
+    // int a,b, tong=0;
+    // cout << "nhap a: "; cin >>a;
+    // cout << "nhap b: "; cin >>b;
+    // tong= a + b;
+    // cout << a << " + " << b << " = " << tong<<endl;
     // cout <<"tong cua 2 so laf : " << tong;
+    // int so=1;
+    // while (so <= 100)
+    // {
+    //     if(so % 2 == 1)
+    //     {
+    //         cout<< so << "\t";
+    //     }
+    //     if (so % 10 == 0)
+    //     {
+    //         cout<< "\n";
+    //     }
+        
+    //     so = so + 1;
+    // }
+    sochan();
+    cout<< setfill('-');
+    cout << setw(35) << "-" << endl;
+    sole();
    return 0;
 }
