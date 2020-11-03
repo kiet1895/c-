3e50785789c41c45c63486e94a5f4dd1574757a6
c++ <kiet1895@gmail.com>
2020-11-03 22:55:42 +0700

    update vong lap neu a = 0 va lenh clear

--- ptBachai.cpp
+++ ptBachai.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <string>
 using namespace std;
 
 int ptbachai(float a, float b, float c)
@@ -8,42 +9,53 @@ int ptbachai(float a, float b, float c)
     delta = b * b - (4 * a * c);
     if (delta < 0)
     {
-        cout << "Phuong trinh vo nghiem"<< endl;
+        cout << "Phuong trinh vo nghiem" << endl;
     }
     float x, y;
     if (delta == 0)
     {
         x = -b / (2 * a);
-        cout << "phuong trinh co nghiem x = y : " << x<< endl;
+        cout << "phuong trinh co nghiem x = y : " << x << endl;
     }
     if (delta > 0)
     {
         x = (-b + sqrt(delta)) / (2 * a);
         y = (-b - sqrt(delta)) / (2 * a);
-        cout << "phuong trinh co nghiem x: " << x<<endl;
-        cout << "phuong trinh co nghiem y: " << y<<endl;
+        cout << "phuong trinh co nghiem x: " << x << endl;
+        cout << "phuong trinh co nghiem y: " << y << endl;
     }
     return 0;
 }
 int main()
 {
-    float a, b, c;
-    cout << "moi nhap a,b,c \n ";
-    cout << "moi nhap a ";
-    cin>>a;
-    cout << "moi nhap b ";
-    cin>>b;
-    cout << "moi nhap c ";
-    cin>>c;
-    if (a == 0)
+    float b, c;
+    float a(0);
+    // cout << "moi nhap a,b,c \n ";
+    // cout << "moi nhap a ";
+    // cin >> a;
+    // cout << "moi nhap b ";
+    // cin >> b;
+    // cout << "moi nhap c ";
+    // cin >> c;
+    // if (a == 0)
+    // {
+    //     cout << "moi nhap a khac 0";
+    // }
+    // else
+    // {
+    //     ptbachai(a, b, c);
+    // }
+    do
     {
-        cout << "moi nhap a khac 0";
-    }
-    else
-    {
-         ptbachai(a, b, c);
-    }
-    
-
+        system("clear");
+        cout << "Moi nhap a b c với a khác 0 " << endl;
+        cout << "moi nhap a: ";
+        cin >> a;
+        cout << "moi nhap b: ";
+        cin >> b;
+        cout << "moi nhap c: ";
+        cin >> c;
+    } while (!a);
+    ptbachai(a, b, c);
     return 0;
 }
